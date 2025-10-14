5600bfbf42f51ed77782174550bd516697893fe3
Khalil <kjamil2004@gmail.com>
2025-10-14 21:51:53 +0300

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TabsDemo } from './pages/frontpage'
+import { Frontpage } from './pages/frontpage'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TabsDemo />
+    <Frontpage />
   </StrictMode>,
 )
