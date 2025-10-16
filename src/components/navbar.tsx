515f9f6a7d1940ddf39803b796d2e7b88ae65f33
Rami-Nasr04 <rami55education@gmail.com>
2025-10-17 00:55:14 +0200

    fixed the positioning in items
    centered logo and positioned the divs. even on small screens and
    more work to go.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,40 +9,44 @@ const linkClass = ({ isActive }: { isActive: boolean }) =>
         : 'text-black hover:bg-gray-500 hover:text-white rounded-md px-3 py-2'; // false case
 
   return (
-    <nav className='bg-white border-b border-green-500'>
-      <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-        <div className='flex h-20 items-center justify-between'>
-          <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            <div className="w-full relative flex items-center">
-              {/* Left links */}
-              <div className="flex items-center space-x-2">
-                <NavLink to="/" className={linkClass}>
-                  Carpool
-                </NavLink>
-                <NavLink to="/Frontpage" className={linkClass}>
-                  Bus
-                </NavLink>
-              </div>
+      <nav className="bg-white border-b border-green-500">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        
+        {/* Main container: 3 equal sections */}
+        <div className="flex items-center justify-between h-24">
 
-              {/* Centered logo */}
-              <div className="absolute left-1/2 transform -translate-x-1/2 pointer-events-none">
-                <NavLink to="/" className="pointer-events-auto flex flex-shrink-0 items-center">
-                  <img className="h-10 w-auto" src={logo} alt="Wasselni" />
-                </NavLink>
-              </div>
-            </div>
-            <div className='md:ml-auto'>
-              <div className='flex space-x-2'>
+          
+          {/* Left side links */}
+          <div className="flex items-center space-x-3">
+            <NavLink to="/" className={linkClass}>
+              Carpool
+            </NavLink>
+            <NavLink to="/Frontpage" className={linkClass}>
+              Bus
+            </NavLink>
 
-                <NavLink to='/Search' className={linkClass}>
-                  Search
-                </NavLink>
-                <NavLink to='/add-job' className={linkClass}>
-                  Publish a ride
-                </NavLink>
-              </div>
-            </div>
           </div>
+
+          {/* Center logo */}
+          <div className="flex justify-center items-center shrink-20">
+            <NavLink to="/">
+              <img className="h-auto w-auto min-h-[20px] max-h-[500px]" src={logo} alt="Wasselni" />
+            </NavLink>
+          </div>
+
+          {/* Right side links */}
+          <div className="flex items-center space-x-3">
+            <NavLink to="/Search" className={linkClass}>
+              Search
+            </NavLink>
+            <NavLink to="/add-job" className={linkClass}>
+              Publish a ride
+            </NavLink>            
+            <NavLink to="/Account" className={linkClass}>
+              Account
+            </NavLink>
+          </div>
+
         </div>
       </div>
     </nav>
