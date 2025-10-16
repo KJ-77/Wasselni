df58a601713108a2fb7a8eddf5d8474c65a7866c
Rami-Nasr04 <rami55education@gmail.com>
2025-10-17 00:14:25 +0200

    working with the NavBar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,33 +1,44 @@
 import { NavLink } from 'react-router-dom';
-// import logo from '../assets/images/logo.png';
+
+import logo from '../assets/logo/wasselni_logo_transparent.png';
 
 const Navbar = () => {
 const linkClass = ({ isActive }: { isActive: boolean }) =>
     isActive
-        ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-        : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+        ? 'bg-gray-200 text-black font-bold hover:bg-gray-500 hover:text-black rounded-md px-3 py-2'  // true case
+        : 'text-black hover:bg-gray-500 hover:text-white rounded-md px-3 py-2'; // false case
 
   return (
-    <nav className='bg-indigo-700 border-b border-indigo-500'>
+    <nav className='bg-white border-b border-green-500'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'>
-              {/* <img className='h-10 w-auto' src={logo} alt='React Jobs' /> */}
-              <span className='hidden md:block text-white text-2xl font-bold ml-2'>
-                React Jobs
-              </span>
-            </NavLink>
+            <div className="w-full relative flex items-center">
+              {/* Left links */}
+              <div className="flex items-center space-x-2">
+                <NavLink to="/" className={linkClass}>
+                  Carpool
+                </NavLink>
+                <NavLink to="/Frontpage" className={linkClass}>
+                  Bus
+                </NavLink>
+              </div>
+
+              {/* Centered logo */}
+              <div className="absolute left-1/2 transform -translate-x-1/2 pointer-events-none">
+                <NavLink to="/" className="pointer-events-auto flex flex-shrink-0 items-center">
+                  <img className="h-10 w-auto" src={logo} alt="Wasselni" />
+                </NavLink>
+              </div>
+            </div>
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
-                <NavLink to='/' className={linkClass}>
-                  Home
-                </NavLink>
-                <NavLink to='/jobs' className={linkClass}>
-                  Jobs
+
+                <NavLink to='/Search' className={linkClass}>
+                  Search
                 </NavLink>
                 <NavLink to='/add-job' className={linkClass}>
-                  Add Job
+                  Publish a ride
                 </NavLink>
               </div>
             </div>
