f9039782d20b928d065be52c0b0dd312ec6771a3
Rami-Nasr04 <rami55education@gmail.com>
2025-10-17 01:11:13 +0200

    resolved some issues with navbar
    there are more waiting for some help, i need a break

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,8 +5,8 @@ import logo from '../assets/logo/wasselni_logo_transparent.png';
 const Navbar = () => {
 const linkClass = ({ isActive }: { isActive: boolean }) =>
     isActive
-        ? 'bg-gray-200 text-black font-bold hover:bg-gray-500 hover:text-black rounded-md px-3 py-2'  // true case
-        : 'text-black hover:bg-gray-500 hover:text-white rounded-md px-3 py-2'; // false case
+        ? 'bg-gray-200 text-black font-bold hover:bg-gray-500 hover:text-black rounded-md px-3 py-2 whitespace-nowrap'  // true case
+        : 'text-black hover:bg-gray-500 hover:text-white rounded-md px-3 py-2 whitespace-nowrap'; // false case
 
   return (
       <nav className="bg-white border-b border-green-500">
@@ -28,17 +28,18 @@ const linkClass = ({ isActive }: { isActive: boolean }) =>
           </div>
 
           {/* Center logo */}
-          <div className="flex justify-center items-center shrink-20">
+          <div className="flex justify-center items-center">
             <NavLink to="/">
-              <img className="h-auto w-auto min-h-[20px] max-h-[500px]" src={logo} alt="Wasselni" />
+              <img className="h-auto w-auto min-h-[40px] max-h-[500px]  min-w-sm-[400px]" src={logo} alt="Wasselni" />
             </NavLink>
           </div>
 
           {/* Right side links */}
-          <div className="flex items-center space-x-3">
-            <NavLink to="/Search" className={linkClass}>
-              Search
-            </NavLink>
+          <div className="flex items-center space-x-3 ">
+          <div className="hidden sm:flex">
+             <NavLink to="/Search" className={linkClass}>
+             Search</NavLink>
+          </div>
             <NavLink to="/add-job" className={linkClass}>
               Publish a ride
             </NavLink>            
