f91773e53ed585d70a5cfc8bac04ac91ca6263fb
Rami-Nasr04 <rami55education@gmail.com>
2025-10-18 17:42:30 +0200

    added functions to the optionForm

--- src/components/optionForm.tsx
+++ src/components/optionForm.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState} from 'react'
+import {FaUser, FaCalendarAlt, FaDotCircle} from 'react-icons/fa'
 
 function optionForm() {
+  const [passengers, setPassengers] = useState(1);
+  const [open, setOpen] = useState(false);
+
+  const increment = () => {
+    setPassengers(passengers + 1);
+  }
+  const decrement = () => {
+    if (passengers > 1) {
+      setPassengers(passengers - 1);
+    }
+    else {
+      setPassengers(1);
+    }
+  }
+
+
   return (
-    <div>
+    <div className='flex items-center justify-between w-full px-4 py-2 border rounded-lg'>
       
     </div>
   )
